74c461668602e90abb39de27ebd809311c427cd2
AryanRagupathy <67152544+AryanRagupathy@users.noreply.github.com>
2020-07-16 09:08:49 +0530

    all files

---
+++ sketch.js
@@ -0,0 +1,65 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+var bin1,bin2,bin3
+var ball1;
+var engine,world
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+	
+	engine = Engine.create();
+    world = engine.world;
+
+	//Create the Bodies Here.
+    bin1 = new Bin(675,640,100,80);
+    bin2 = new Bin2(630,640,10,80);
+    bin3 = new Bin2(720,640,10,80);
+    bin4 = new Bin2(675,675,90,10);
+	ground = new Ground(400,690,800,20);
+	ball1 = new Ball(100,500)
+  launcher= new Launcher(ball1.body,{x:100,y:400});
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  Engine.update(engine);	
+  rectMode(CENTER);
+  background(255);
+  
+  ball1.display()
+  bin1.display();
+  bin2.display();
+  bin3.display();
+  bin4.display();
+  
+ 
+  ground.display();
+  launcher.display();
+
+  drawSprites();
+ 
+}
+
+/*function keyPressed(){
+  if(keyCode === UP_ARROW){
+    Matter.Body.applyForce(ball1.body, ball1.body.position, {x:5,y:-8})
+  }
+}
+*/
+function mouseDragged(){
+  Body.setPosition(ball1.body,{x:mouseX,y:mouseY})
+  }
+  function mouseReleased(){
+  launcher.fly();
+  }
